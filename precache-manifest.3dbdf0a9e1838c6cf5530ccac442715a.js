0a5d2b7fe7adf43cac7bb08830ed10c70ea03fa1
Januar Andaria <janucaria@gmail.com>
2019-04-26 00:19:26 +0800

    Updates

---
+++ precache-manifest.3dbdf0a9e1838c6cf5530ccac442715a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dee6ccd2b3255b2932a065ba35d40dcc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab8cd0edd10d7e682d00",
+    "url": "/static/css/main.e5592087.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "ab8cd0edd10d7e682d00",
+    "url": "/static/js/main.ed6b0530.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
